bdb63fcda885d1b60abe4d7734d13fd4db074dd3
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2018-11-26 22:31:25 -0200

    Camada de Dominio Camada de repositorio

---
+++ Oficina.Dominio/Cambio.cs
@@ -0,0 +1,9 @@
+﻿namespace Oficina.Dominio
+{
+    public enum Cambio
+    {
+        Manual = 1,
+        Automatico = 2,
+        SemiAutomatico = 3
+    }
+}
---
+++ Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Oficina.Dominio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina.Repositorios.SistemaArquivos
+{
+    public class CorRepositorio
+    {
+        public List<Cor>Selecionar()
+        {
+            var cores = new List<Cor>();
+
+            //para cada linha que esta dentro do arquivo de texto 
+            foreach (var linha in File.ReadAllLines("Dados\\Cor.txt"))
+            {
+                //Cor cor = new Cor();
+                var cor = new Cor();
+
+                cor.Id = Convert.ToInt32(linha.Substring(0, 5));
+                cor.Nome = linha.Substring(5);
+
+                cores.Add(cor);
+            }
+
+
+            return cores;
+
+        }
+    }
+}
