0b4fdac7697b7b65def01b0a90de0c7b1ecacab9
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2018-11-27 22:24:09 -0200

    Selecionar TodaLista Selecionar por ID

--- Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
+++ Oficina.Repositorios.SistemaArquivos/CorRepositorio.cs
@@ -10,14 +10,14 @@ namespace Oficina.Repositorios.SistemaArquivos
 {
     public class CorRepositorio
     {
-        public List<Cor>Selecionar()
+        const string caminhoArquivo = @"Dados\Cor.txt";
+
+        public List<Cor> Selecionar()
         {
             var cores = new List<Cor>();
 
-            //para cada linha que esta dentro do arquivo de texto 
-            foreach (var linha in File.ReadAllLines("Dados\\Cor.txt"))
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
             {
-                //Cor cor = new Cor();
                 var cor = new Cor();
 
                 cor.Id = Convert.ToInt32(linha.Substring(0, 5));
@@ -26,9 +26,29 @@ namespace Oficina.Repositorios.SistemaArquivos
                 cores.Add(cor);
             }
 
-
             return cores;
+        }
 
+        public Cor Selecionar(int id)
+        {
+            //int? x = null;
+             Cor cor = null;
+
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
+            {
+                var linhaId = linha.Substring(0, 5);
+
+                if (Convert.ToInt32(linhaId) == id)
+                {
+                    cor = new Cor();
+                    cor.Id = Convert.ToInt32(linha.Substring(0, 5));
+                    cor.Nome = linha.Substring(5);
+
+                    break;
+                }
+            }
+
+            return cor;
         }
     }
 }
---
+++ Oficina.Repositorios.SistemaArquivos/MarcaRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using Oficina.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina.Repositorios.SistemaArquivos
+{
+    public class MarcaRepositorio
+    {
+        private string caminhoArquivo = ConfigurationManager.AppSettings["caminhoArquivoMarca"];
+
+        public List<Marca> Selecionar()
+        {
+            var marcas = new List<Marca>();
+
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
+            {
+                var propriedades = linha.Split('|');
+
+                var marca = new Marca();
+
+                marca.Id = Convert.ToInt32(propriedades[0]);
+                marca.Nome = propriedades[1];
+
+                marcas.Add(marca);
+            }
+
+            return marcas;
+        }
+
+        public Marca Selecionar(int id)
+        {
+            //int? x = null;
+            Marca marca = null;
+
+            foreach (var linha in File.ReadAllLines(caminhoArquivo))
+            {
+                var propriedades = linha.Split('|');
+
+                var linhaId = propriedades[0]; //linha.Substring(0, 1);
+
+                if (Convert.ToInt32(linhaId) == id)
+                {
+                    marca = new Marca();
+                    marca.Id = Convert.ToInt32(propriedades[0]);
+                    marca.Nome = propriedades[1];
+
+                    break;
+                }
+            }
+
+            return marca;
+        }
+    }
+}
