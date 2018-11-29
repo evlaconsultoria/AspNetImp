838dbd30525b2e3e91cf6001f63e57d8ca00cf65
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2018-11-28 22:34:06 -0200

    Inserir

---
+++ Oficina.Repositorios.SistemaArquivosTests/ModeloRepositorioTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oficina.Repositorios.SistemaArquivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina.Repositorios.SistemaArquivos.Tests
+{
+    [TestClass()]
+    public class ModeloRepositorioTests
+    {
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void SelecionarTest(int marcaId)
+        {
+
+            var reposiotior = new ModeloRepositorio();
+
+            var modelos = reposiotior.SelecionarPorMarca(marcaId);
+
+            foreach (var modelo in modelos)
+            {
+                Console.WriteLine($"{modelo.Id}: {modelo.Nome} ({modelo.Marca.Nome})");
+            }
+
+
+        }
+
+        [TestMethod()]
+        [DataRow(1)]
+        public void SelecionarTest1(int id)
+        {
+            Assert.IsTrue(new ModeloRepositorio().Selecionar(id).Nome=="Fox");
+        }
+    }
+}
---
+++ Oficina.Repositorios.SistemaArquivosTests/VeiculoRepositorioTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Oficina.Dominio;
+using Oficina.Repositorios.SistemaArquivos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oficina.Repositorios.SistemaArquivos.Tests
+{
+    [TestClass()]
+    public class VeiculoRepositorioTests
+    {
+        [TestMethod()]
+        public void InserirTest()
+        {
+            Veiculo veiculo = new Veiculo();
+
+            veiculo.Ano = 2014;
+            veiculo.Placa = "EBS0795";
+            veiculo.Cambio = Cambio.Automatico;
+            veiculo.Combustivel = Combustivel.Flex;
+            veiculo.Observacao = "Observacao";
+
+            veiculo.Cor = new CorRepositorio().Selecionar(1);
+            veiculo.Modelo = new ModeloRepositorio().Selecionar(1);
+                                 
+            
+
+            new VeiculoRepositorio().Inserir(veiculo);
+        }
+    }
+}
